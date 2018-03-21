02d1e3af47878fb769cb89cd3f311877b9935c1c
xiebaiyuan <xiebaiyuan@foxmail.com>
2018-03-21 23:56:54 +0800

    appium demo by test

--- app/src/test/java/com/xiebaiyuan/appiumdemoforandroid/ExampleUnitTest.java
+++ app/src/test/java/com/xiebaiyuan/appiumdemoforandroid/ExampleUnitTest.java
@@ -1,8 +1,14 @@
 package com.xiebaiyuan.appiumdemoforandroid;
 
 import org.junit.Test;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
-import static org.junit.Assert.*;
+import java.net.URL;
+
+import io.appium.java_client.AppiumDriver;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +19,18 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+
+    @Test
+    public void testForAppium() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(CapabilityType.BROWSER_NAME, "");//这句不是必须的
+        capabilities.setCapability("deviceName", "Android Emulator");
+        capabilities.setCapability("platformVersion", "7.0");
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("appPackage", "com.android.calculator2");
+        capabilities.setCapability("appActivity", ".Calculator");
+        AppiumDriver driver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
 }
